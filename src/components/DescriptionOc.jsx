9388b70a8786cb39d21bce62a6ec716baf3a2aac
jose ramon arias borges <jaborges666@gmail.com>
2025-07-29 16:34:22 -0500

    se completo la logica para agregar un numero de orden de produccion

--- src/components/DescriptionOc.jsx
+++ src/components/DescriptionOc.jsx
@@ -45,83 +45,81 @@ function DescriptionOc() {
     setDescriptionsSerie([...descriptionsSerie, { id: newId, text: '', input: '' }]);
   };
 
+  const addNewPair = () => {
+    const newId = Math.max(
+      descriptions[descriptions.length - 1].id,
+      descriptionsSerie[descriptionsSerie.length - 1].id
+    ) + 1;
+
+    setDescriptions([...descriptions, { id: newId, text: '', input: '' }]);
+    setDescriptionsSerie([...descriptionsSerie, { id: newId, text: '', input: '' }]);
+  };
+
   return (
     <div>
-      {descriptions.map((desc) => (
-        <div key={desc.id}>
-          <div className="container-align-left">
-            <textarea
-              name={`description-${desc.id}`}
-              placeholder="Agregue la descripción"
-              value={desc.input}
-              onChange={(e) => handleInputChange(desc.id, e.target.value)}
-              rows={4}
-              className=""
-            />
-            <button
-              type="button"
-              onClick={() => handleButtonClick(desc.id)}
-            >
-              Enviar
-            </button>
-          </div>
-          <div className="container-align-left">
-            <p className="description-oc">
-              {desc.id}- &nbsp;&nbsp; MOD: {desc.text || ' '}
-            </p>
-          </div>
-        </div>
-      ))}
-      {/* en contruccion */}
+      {descriptions.map((desc) => {
+        const serie = descriptionsSerie.find(s => s.id === desc.id);
 
-
-      {/* <div > */}
-        {descriptionsSerie.map((descSerie) => (
-          <div key={descSerie.id}>
+        return (
+          <div key={desc.id}>
             <div className="container-align-left">
               <textarea
-                name={`description-${descSerie.id}`}
-                placeholder="agregue el numero de orden de producción"
-                value={descSerie.input}
-                onChange={(e) => handleInputChangeSerie(descSerie.id, e.target.value)}
+                placeholder="Agregue la descripción"
+                value={desc.input}
+                onChange={(e) => handleInputChange(desc.id, e.target.value)}
                 rows={4}
-                className="description-textarea"
               />
               <button
                 type="button"
-                onClick={() => handleButtonClickSerie(descSerie.id)}
+                onClick={() => handleButtonClick(desc.id)}
               >
                 Enviar
               </button>
             </div>
-              <div className="container-align-left">
-                <p className="description-oc">
-                  {descSerie.text ? `SERIE #: ${descSerie.text}` : ''}
-                  {/* {descSerie.id} {descSerie.text || ' hello world'} */}
-                </p>
-              </div>
+            <div className="container-align-left">
+              <p className="description-oc">
+                {desc.id}- &nbsp;&nbsp; MOD: {desc.text || ' '}
+              </p>
+            </div>
+            {serie && (
+              <>
+                <div className="container-align-left">
+                  <textarea
+                    placeholder="agregue el numero de orden de producción"
+                    value={serie.input}
+                    onChange={(e) => handleInputChangeSerie(serie.id, e.target.value)}
+                    rows={4}
+                    className="description-textarea"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => handleButtonClickSerie(serie.id)}
+                  >
+                    Enviar
+                  </button>
+                </div>
+                <div className="container-align-left">
+                  <p className="description-oc">
+                    {serie.text ? `SERIE #: ${serie.text}` : ''}
+                  </p>
+                </div>
+              </>
+            )}
           </div>
-        ))}
-        {/* </div>   */}
-
-
-        {/* Fin de contruccion */}
-
-        {descriptions.length < MAX_DESCRIPTIONS && (
-          <div className="container-align-left">
-            <button
-              type="button"
-              onClick={() => {
-                      addNewDescriptionField();
-                      addNewDescriptionFieldSerie();
-                      }}
-              className="add-button"
-            >
-              +
-            </button>
-          </div>
-        )}
-      </div>
+        );
+      })}
+      {descriptions.length < MAX_DESCRIPTIONS && (
+        <div className="container-align-left">
+          <button
+            type="button"
+            onClick={addNewPair}
+            className="add-button"
+          >
+            +
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
 
