10146df8633c8297924fb2a50e9aa96234f575b3
jose ramon arias borges <jaborges666@gmail.com>
2025-12-07 07:44:12 -0500

    feat: inplementando un layaut responcive

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,82 +1,112 @@
 import Image from "next/image";
 // import { Geist, Geist_Mono } from "next/font/google";
-import TodayDate from "@/components/TodayDate";
-import Print from "@/components/Print";
-import FormNameClient from "@/components/FormNameClient";
-import FormOc from "@/components/FormOc";
-import DescriptionOc from "@/components/DescriptionOc";
-import Pagination from "@/components/Pagination";
+// import TodayDate from "@/components/TodayDate";
+// import Print from "@/components/Print";
+// import FormNameClient from "@/components/FormNameClient";
+// import FormOc from "@/components/FormOc";
+// import DescriptionOc from "@/components/DescriptionOc";
+// import Pagination from "@/components/Pagination";
 
 export default function Home() {
 
 
   return (
-    <div>
-      <header className="header">
-        <div className="header-container">
-          <div className="logo-container">
-            <Image
-              src="/img/atecno.png"
-              alt="ATECNO Logo"
-              width={300}
-              height={120}
-              priority
-            />
-          </div>
-          <div className="header-text">
-            <h1><strong>ACCI TECNOLOGY S.A. DE CV</strong></h1>
-            <div className="header-info">
-              <p>Cto. Privada Residencial Nichupte. SM 512, MZ-12, LT-06.CP:77534. Cancún.</p>
-              <p>Benito Juarez. Quintana Roo</p>
-              <p>RFC: ATE031119PEA</p>
-            </div>
-          </div>
-        </div>
+    <div className="container">
+      <header >
+        <Image src="/img/atecno.png" width={300} height={120} alt="logo_atecno" className="ImageLogo" />
+        <h1 className="titlePrimaryUnderline textAlign"><strong>ACCI TECNOLOGY S.A. DE CV</strong></h1>
       </header>
-      <div className="header-divider">
-        {/* <button className="dividing-line" aria-hidden="true"></button> */}
-        <hr className="hr-dividing-line" />
-      </div>
-      <main>
+      <article className="article">
         <div>
-          <div className="date-container">
-            <p className="date-text">
-              <TodayDate />
-            </p>
-          </div>
-          <div>
-            <h2 className="title">ACTA DE ENTREGA</h2>
-          </div>
-          <div className="container-align-left">
-            <FormNameClient currentPage={1} />
-          </div>
-          <div className="container-align-left">
-            <p>Se hace entrega del siguiente material:</p>
-          </div>
-          <div className="container-align-left">
-            <FormOc currentPage={1} />
-          </div>
-          <div className="container-textarea">
-            <DescriptionOc currentPage={1} />
-          </div>
-          <div className="firm-container">
-            <div className="container-align-left">
-              <p>RECIBE CONFORME: </p>
-              <button className="button-lain" id="recibe" ></button>
-              <p className="no-print"></p>
-            </div>
-            <br />
-            <div className="container-align-left">
-              <p>ENTREGA:</p>
-              <button className="button-lain" id="entrega"></button>
-              <p className="no-print"></p>
-            </div>
-          </div>
+          <p>Cto. Privada Residencial Nichupte. SM 512, MZ-12, LT-06.CP:77534. Cancún.</p>
+          <p>Benito Juarez. Quintana Roo</p>
+          <p>RFC: ATE031119PEA</p>
+          <hr className="hrDividingLine" />
         </div>
-      </main>
-      <br />
-      <Print />
-      <Pagination currentPage={1} />
+        <div>
+          {/* <p>
+            <TodayDate />
+          </p> */}
+        </div>
+        <div>
+          <h1>ACTA DE ENTREGA</h1>
+        </div>
+        {/* <FormNameClient /> */}
+        <p>Se hace entrega del siguiente material:</p>
+        {/* <FormOc /> */}
+        {/* <DescriptionOc /> */}
+      </article>
+      <footer>
+        <p> hola mundo</p>
+      </footer>
     </div>
+
+    // <div>
+    //   <header className="header">
+    //     <div className="header-container">
+    //       <div className="logo-container">
+    //         <Image
+    //           src="/img/atecno.png"
+    //           alt="ATECNO Logo"
+    //           width={300}
+    //           height={120}
+    //           priority
+    //         />
+    //       </div>
+    //       <div className="header-text">
+    //         <h1><strong>ACCI TECNOLOGY S.A. DE CV</strong></h1>
+    //         <div className="header-info">
+    //           <p>Cto. Privada Residencial Nichupte. SM 512, MZ-12, LT-06.CP:77534. Cancún.</p>
+    //           <p>Benito Juarez. Quintana Roo</p>
+    //           <p>RFC: ATE031119PEA</p>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </header>
+    //   <div className="header-divider">
+    //     {/* <button className="dividing-line" aria-hidden="true"></button> */}
+    //     <hr className="hr-dividing-line" />
+    //   </div>
+    //   <main>
+    //     <div>
+    //       <div className="date-container">
+    //         <p className="date-text">
+    //           <TodayDate />
+    //         </p>
+    //       </div>
+    //       <div>
+    //         <h2 className="title">ACTA DE ENTREGA</h2>
+    //       </div>
+    //       <div className="container-align-left">
+    //         <FormNameClient currentPage={1} />
+    //       </div>
+    //       <div className="container-align-left">
+    //         <p>Se hace entrega del siguiente material:</p>
+    //       </div>
+    //       <div className="container-align-left">
+    //         <FormOc currentPage={1} />
+    //       </div>
+    //       <div className="container-textarea">
+    //         <DescriptionOc currentPage={1} />
+    //       </div>
+    //       <div className="firm-container">
+    //         <div className="container-align-left">
+    //           <p>RECIBE CONFORME: </p>
+    //           <button className="button-lain" id="recibe" ></button>
+    //           <p className="no-print"></p>
+    //         </div>
+    //         <br />
+    //         <div className="container-align-left">
+    //           <p>ENTREGA:</p>
+    //           <button className="button-lain" id="entrega"></button>
+    //           <p className="no-print"></p>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </main>
+    //   <br />
+    //   <Print />
+    //   <Pagination currentPage={1} />
+    // </div>
   );
 }
